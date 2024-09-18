958a46db08ce8c4a8d633c8a5b093e6931b3f35c
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-09-18 09:50:26 +0700

    back end get room types functionality

--- backend/lakeSide-hotel/src/main/java/com/example/lakeSide_hotel/controller/RoomController.java
+++ backend/lakeSide-hotel/src/main/java/com/example/lakeSide_hotel/controller/RoomController.java
@@ -5,15 +5,13 @@ import com.example.lakeSide_hotel.response.RoomResponse;
 import com.example.lakeSide_hotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +27,11 @@ public class RoomController {
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
 
         return ResponseEntity.ok(response);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes(){
+        return roomService.getAllRoomTypes();
     }
 }
--- backend/lakeSide-hotel/src/main/java/com/example/lakeSide_hotel/repository/RoomRepository.java
+++ backend/lakeSide-hotel/src/main/java/com/example/lakeSide_hotel/repository/RoomRepository.java
@@ -2,6 +2,12 @@ package com.example.lakeSide_hotel.repository;
 
 import com.example.lakeSide_hotel.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
